45796e75661eb1f4b09996493d65ef08c2a2a3b5
mausar17 <mausar_17@hotmail.com>
2021-07-07 02:43:11 -0500

    Add project files.

---
+++ Guys/Guy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace Guys
+{
+    class Guy
+    {
+        public string name = "";
+        public int cashOnGuy = 0;
+        /// <summary>
+        /// Writes my name and the amount of cash I have to the console.
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine("My name is " + name + ". I have $" + cashOnGuy + ".");
+        }
+        /// <summary>
+        /// Gives some of my cash, removing it from my wallet (or printing a message
+        /// if I don't have enough cash).
+        /// </summary>
+        /// <param name="cashGiven"></param>
+        /// <returns>The amount of cash removed from my wallet, or 0 if I don't
+        /// have enough cash (or if the amount is invalid).</returns>
+        public int GiveCash(int cashGiven)
+        {
+            if (cashGiven <= 0)
+            {
+                Console.WriteLine(name + " says: " + cashGiven + " isn't a valid amount.");
+                return 0;
+            }
+
+            if (cashGiven > cashOnGuy)
+            {
+                Console.WriteLine(name + " says: I don't have enough enough cash to give you " + cashGiven);
+                return 0;
+            }
+            cashOnGuy -= cashGiven;
+            return cashGiven;
+        }
+        /// <summary>
+        /// Receive some cash, adding it to my wallet (or printing
+        /// a message to the console if the amount is invalid).
+        /// </summary>
+        /// <param name="cashReceived">Amount of cash to give. </param>
+        public void ReceiveCash(int cashReceived)
+        {
+            if (cashReceived <= 0)
+            {
+                Console.WriteLine(name + " says: " + cashReceived + " isn't an amount I'll take...");
+            }
+            else
+            {
+                cashOnGuy += cashReceived;
+            }
+        }
+    }
+}
---
+++ Guys/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Guys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Create a new Guy object in a variable called joe
+                // Set its Name field to "Joe"
+                // Set its Cash field to 50
+                Guy joe = new Guy() { cashOnGuy = 50, name = "Joe" };
+                // Create a new Guy object in a variable called bob
+                // Set its Name field to "Bob"
+                // Set its Cash field to 100
+                Guy bob = new Guy() { cashOnGuy = 100, name = "Bob" };
+                while (true)
+                {
+                    // Call the WriteMyInfo methods for each Guy object
+                    joe.WriteMyInfo();
+                    bob.WriteMyInfo();
+
+                    Console.Write("Enter an amount: ");
+                    string howMuch = Console.ReadLine();
+                    
+                    //Blank to exit
+                    if (howMuch == "") return;
+                    // Use int.TryParse to try to convert the howMuch string to an int
+                    // if it was successful (just like you did earlier in the chapter)
+                    if (int.TryParse(howMuch, out int amount))
+                    {
+                        Console.Write("Who should give the cash: ");
+                        string whichGuy = Console.ReadLine();
+                        if (whichGuy == "Joe")
+                        {
+                            // Call the joe object's GiveCash method and save the results
+                            joe.GiveCash(amount);
+                            // Call the bob object's ReceiveCash method with the saved results
+                            bob.ReceiveCash(amount);
+                        }
+                        else if (whichGuy == "Bob")
+                        {
+                            // Call the bob object's GiveCash method and save the results
+                            bob.GiveCash(amount);
+                            // Call the joe object's ReceiveCash method with the saved results
+                            joe.ReceiveCash(amount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter an amount (or a blank line to exit).");
+                    }
+                }
+            }
+        }
+    }
+}
